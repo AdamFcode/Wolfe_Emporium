746743d963dcd8c3dcb299d2423efb5425f465b1
Adam Feehely <adamfeehely@gmail.com>
2024-12-27 11:03:11 +0000

    add functionality to view items in cart and provide total price to user

---
+++ cart/contexts.py
@@ -0,0 +1,29 @@
+from decimal import Decimal
+from django.conf import settings
+from django.shortcuts import get_object_or_404
+from books.models import Book
+
+def cart_contents(request):
+
+    cart_items = []
+    total = 0
+    book_count = 0
+    cart = request.session.get('cart', {})
+
+    for item_id, quantity in cart.items():
+        book = get_object_or_404(Book, pk=item_id)
+        total += quantity * book.price
+        book_count += quantity
+        cart_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'book': book,
+        })
+    
+    context = {
+        'cart_items': cart_items,
+        'total': total,
+        'book_count': book_count,
+    }
+
+    return context
