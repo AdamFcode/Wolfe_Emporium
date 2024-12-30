5705b9b92ae919f92509fa1b91ec34a7536c0607
Adam Feehely <adamfeehely@gmail.com>
2024-12-30 16:56:50 +0000

    add checkout summary to checkout.html

--- wolfe_emporium/settings.py
+++ wolfe_emporium/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'books',
     'cart',
     'checkout',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -59,6 +60,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'wolfe_emporium.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -74,6 +77,10 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'cart.contexts.cart_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
