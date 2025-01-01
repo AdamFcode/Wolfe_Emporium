7984a5cc1c338da3b9715909bed704c77f3d88b1
Adam Feehely <adamfeehely@gmail.com>
2025-01-01 17:22:05 +0000

    add initial payment functionality to checkout app via stripe

--- checkout/views.py
+++ checkout/views.py
@@ -1,20 +1,37 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 from .forms import OrderForm
+from cart.contexts import cart_contents
 
+import stripe
 # Create your views here.
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
     cart = request.session.get('cart', {})
     if not cart:
         messages.error(request, "Your cart appears to be empty.")
         return redirect(reverse('books'))
 
+    current_cart = cart_contents(request)
+    total = current_cart['total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
     order_form = OrderForm()
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. \
+            Please set it in your environment?')
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51QcTDNIdri0maLGSveyWElwhjEXH7aiD9IQpmIhrkrbGpHDGAZAnxAbCB3UOEJUOLnLZf8md8zPizExGn3EoILjn00CbJlMuEd',
-        'client_secret': 'test cient secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
