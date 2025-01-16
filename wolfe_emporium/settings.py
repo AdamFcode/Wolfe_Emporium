d3ce6f20bc382e65c81960a1818818fa04c307c2
Adam Feehely <adamfeehely@gmail.com>
2025-01-16 08:23:58 +0000

    edit debug status in setting

--- wolfe_emporium/settings.py
+++ wolfe_emporium/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = 'DEBUG' in os.environ
 
 ALLOWED_HOSTS = [
     '8000-adamfcode-wolfeemporium-s7cq0cf8o4t.ws.codeinstitute-ide.net',
